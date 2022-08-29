24b2b9983b6b5f4c30f89e28b87a4ff17ebf6ea5
vlad28x <vlad.skibin@mail.ru>
2022-08-30 00:29:20 +0600

    TASK-2: Add an application config

---
+++ src/main/java/com/vlad28x/task2/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.vlad28x.task2.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.vlad28x.task2")
+public class ApplicationConfig {
+}
