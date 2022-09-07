3789c7c4f7127a729110d38f3faf214bc33f6ec3
vlad28x <vlad.skibin@mail.ru>
2022-09-07 22:46:43 +0300

    TASK-3: Add hibernate

---
+++ src/main/java/com/vlad28x/app/config/HibernateConfig.java
@@ -0,0 +1,43 @@
+package com.vlad28x.app.config;
+
+import com.vlad28x.app.entity.Employee;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:/database.properties")
+public class HibernateConfig {
+
+    private final Environment env;
+
+    public HibernateConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    EntityManagerFactory entityManagerFactory() {
+        org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+        configuration.addProperties(hibernateProperties());
+        configuration.addAnnotatedClass(Employee.class);
+        return configuration.buildSessionFactory();
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.connection.driver_class", env.getProperty("hibernate.connection.driver_class"));
+        properties.setProperty("hibernate.connection.url", env.getProperty("hibernate.connection.url"));
+        properties.setProperty("hibernate.connection.username", env.getProperty("hibernate.connection.username"));
+        properties.setProperty("hibernate.connection.password", env.getProperty("hibernate.connection.password"));
+        properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql", "false"));
+        properties.setProperty("hibernate.format_sql", env.getProperty("hibernate.format_sql", "false"));
+        properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto", "none"));
+        return properties;
+    }
+
+}
